e4be53a88a6c741557d2f3adbdd1811c6d70388b
muhd-umer <thisismumer@gmail.com>
2025-02-24 13:03:58 +0500

    Use gemini

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 from pathlib import Path
 
-MODEL_PROVIDER = "openai"
+MODEL_PROVIDER = "gemini"
 
 if MODEL_PROVIDER == "openai":
     MODEL_ID = "openai/gpt-4o-mini"
     MODEL_API_KEY = os.getenv("OPENAI_API_KEY")
 elif MODEL_PROVIDER == "gemini":
-    MODEL_ID = "gemini/gemini-2.0-flash-exp"
+    MODEL_ID = "gemini/gemini-2.0-flash"
     MODEL_API_KEY = os.getenv("GEMINI_API_KEY")
 else:
     raise ValueError("Unsupported MODEL_PROVIDER. Choose 'openai' or 'gemini'.")
