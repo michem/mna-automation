c05907b194aba51b9c700cb89d9b958b1f325459
muhd-umer <thisismumer@gmail.com>
2025-02-24 16:32:44 +0500

    Change alignment

--- app.py
+++ app.py
@@ -301,7 +301,9 @@ def initialize_gemini():
 def main():
     st.set_page_config(page_title="M&A Automation", page_icon="💼", layout="centered")
 
-    st.title("M&A Strategy Assessment System")
+    col1, col2, col3 = st.columns([1, 3, 1])
+    with col2:
+        st.title("M&A Automation")
     st.write("Let's discuss your merger and acquisition strategy.")
 
     if "bot" not in st.session_state:
